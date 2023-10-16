c06793d6dc4f280beae0ad767dc685c123682263
Danny Mulvihill <danny@paella.dev>
2023-10-15 21:42:53 -0500

    save firebase project_id as string in initialize object

--- src/app/utils/firebase.ts
+++ src/app/utils/firebase.ts
@@ -3,7 +3,7 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: "crossmint-unity-demo",
       privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     }),
