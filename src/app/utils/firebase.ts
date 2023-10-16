c8931f621a08a1ce94d9643b15d82e39b75cf739
Danny Mulvihill <danny@paella.dev>
2023-10-15 22:28:33 -0500

    base64 encode private key to fix deployments

--- src/app/utils/firebase.ts
+++ src/app/utils/firebase.ts
@@ -1,10 +1,15 @@
 import admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+if (!firebasePrivateKey) {
+  throw new Error('FIREBASE_PRIVATE_KEY is not set in the environment variables');
+}
+
+if (!admin.apps.length) {admin.initializeApp({
+      
     credential: admin.credential.cert({
-      projectId: "crossmint-unity-demo",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: Buffer.from(firebasePrivateKey, 'base64').toString(),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     }),
   });
